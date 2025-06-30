94921a9d2887b7ba6aff9e07e752c9826a26f2ed
rhythmjalhotra <rhythmjalhotra17@gmail.com>
2025-06-30 18:30:45 +0530

    bj remove

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { TireRackService } from './scrapper/tirerack/tirerack.service';
-import { BjService } from './scrapper/bj/bj.service';
+
 import { WalmartService } from './scrapper/walmart/walmart.service';
 import { DiscountTireService } from './scrapper/discountTire/discountTire.service';
 
@@ -8,7 +8,7 @@ import { DiscountTireService } from './scrapper/discountTire/discountTire.servic
 export class AppService implements OnModuleInit {
   constructor(
     private readonly tireRackService: TireRackService,
-    private readonly bjService: BjService,
+   
     private readonly walmartService: WalmartService,
     private readonly discountTireService:DiscountTireService
   ) {}
